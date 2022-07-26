3e4ce3dd6fc1f029aec9941bdaf3faa221ca0f4c
mmontonerin <merce.montoliu@gmail.com>
2022-07-26 12:54:31 +0200

    fix mistake

--- 01_Gene_prediction/braker_with_RNA_data.sh
+++ 01_Gene_prediction/braker_with_RNA_data.sh
@@ -7,4 +7,4 @@ p="/path/annotation/proteome/sophophora_proteins.fasta"
 rna="/path/annotation/RNA_map/O11Aligned.sortedByCoord.out.bam"
 wdir="/path/annotation/braker_out/O11/"
 
-braker.pl --species=D_paulistorum_O11 --useexisting --genome=$g --prot_seq=$p --bam=$rna --softmasking --cores=8 --etpmode --workingdir=$wdir
+braker.pl --species=D_paulistorum_O11 --genome=$g --prot_seq=$p --bam=$rna --softmasking --cores=8 --etpmode --workingdir=$wdir
