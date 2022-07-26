f55efc6ab840d1b0d9ceda5ef4c8ca97dfe265ab
mmontonerin <merce.montoliu@gmail.com>
2022-07-26 12:51:46 +0200

    braker_with_RNA_data

---
+++ 01_Gene_prediction/braker_with_RNA_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Important, get ~/.gm_key license from genemark in home directory
+#Configure Augustus folder
+
+g="/path/annotation/assemblies/O11_polish_nextdenovo_masked.fasta"
+p="/path/annotation/proteome/sophophora_proteins.fasta"
+rna="/path/annotation/RNA_map/O11Aligned.sortedByCoord.out.bam"
+wdir="/path/annotation/braker_out/O11/"
+
+braker.pl --species=D_paulistorum_O11 --useexisting --genome=$g --prot_seq=$p --bam=$rna --softmasking --cores=8 --etpmode --workingdir=$wdir
