a1f0e6e1cfe96f05ec5aafbc4e40e8755ed6410b
mmontonerin <merce.montoliu@gmail.com>
2022-07-27 10:09:25 +0200

    prepare for iqtree

---
+++ 02_Ortholog_find_and_Phylogenetics/prepare_partition_file_for_IQtree.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+##
+use warnings;
+use strict;
+#Mercè Montoliu-Nerin, July 2022
+###usage: prepare_partition_file_for_iqtree.pl <partition_file> <new_partition_file>
+#
+#Changes a partition file from this (after running gene_stitcher.py in https://github.com/ballesterus/Utensils):
+# = 1-541;
+# = 542-703;
+#To this:
+#AA, part1 = 1-541;
+#AA, part2 = 542-703;
+
+open(PART, "<$ARGV[0]") || die("Usage: prepare_partition_file_for_iqtree.pl <partition_file> <new_partition_file>\n$!,aborted");
+
+open(OUTPUT, ">$ARGV[1]") || die("Usage: prepare_partition_file_for_iqtree.pl <partition_file> <new_partition_file> \n$!,aborted");
+
+my $i = 0;
+
+while (my $line = <PART>)
+{
+        chomp $line;
+        {
+                if ($line =~ /(=.+;)/)
+                {
+                        $i++;
+                        print OUTPUT "AA, part$i $1\n";
+                }
+                else
+                {
+                next;
+                }
+        }
+}
+exit;
